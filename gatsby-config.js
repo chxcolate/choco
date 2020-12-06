27195b408533dc8972edb4181d88a03ccac98912
electricdanwa <electricdanwa@gmail.com>
2020-12-06 16:12:03 -0500

    still trying to fix azup workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,11 +22,5 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/images/`,
-      },
-    },
   ],
 }
